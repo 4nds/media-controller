1cd8232fd7411aeb9f52ed30d929255f4944ff54
4nds <andrej.slapnicar@gmail.com>
2018-03-12 14:23:35 +0100

    Add files via upload

---
+++ receiver.js
@@ -0,0 +1,30 @@
+(function (window, document, undefined) {
+	
+	'use strict';
+	
+	// A function to process messages received by the window.
+	function receiveMessage(messageEle, e) {
+		// Check to make sure that this message came from the correct domain.
+		//if (e.origin !== "http://s.codepen.io")
+		//	return;
+		console.log(e.origin);
+
+		// Update the div element to display the message.
+		messageEle.innerHTML = "Message Received: " + e.data;
+	}
+	
+
+	function main() {
+		// Get a reference to the <div> on the page that will display the message text.
+		var messageEle = document.getElementById('message');
+	
+		// Setup an event listener that calls receiveMessage() when the window receives a new MessageEvent.
+		window.addEventListener('message', receiveMessage.bind(null, messageEle));
+		
+		
+		return;
+	}
+
+	window.addEventListener('load', main, false);
+
+})(window, document);
